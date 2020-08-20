b0d1b03f1f97c173090c01fe2b4333c7748d193d
onumengine <onumdev@gmail.com>
2020-08-20 20:54:49 +0100

    I can now save notes to an SQLite database

--- src/main/java/com/example/notes/databases/NotesDatabase.java
+++ src/main/java/com/example/notes/databases/NotesDatabase.java
@@ -1,11 +1,14 @@
 package com.example.notes.databases;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.example.notes.models.Note;
 
 public class NotesDatabase extends SQLiteOpenHelper
 {
@@ -20,7 +23,7 @@ public class NotesDatabase extends SQLiteOpenHelper
     @Override
     public void onCreate(@NonNull SQLiteDatabase sqLiteDatabase)
     {
-        String tableCreationStatement = "CREATE TABLE NOTES (_id INTEGER PRIMARY KEY AUTOINCREMENT, \n" + "TITLE TEXT, \n" + "BODY TEXT)";
+        String tableCreationStatement = "CREATE TABLE NOTES (_id INTEGER PRIMARY KEY AUTOINCREMENT, \n" + "title TEXT, \n" + "body TEXT)";
         sqLiteDatabase.execSQL(tableCreationStatement);
         sqLiteDatabase.close();
     }
@@ -29,6 +32,22 @@ public class NotesDatabase extends SQLiteOpenHelper
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1)
     {
         String tableDropStatement = "DROP TABLE IF EXISTS NOTES";
+
         sqLiteDatabase.execSQL(tableDropStatement);
+
+        onCreate(sqLiteDatabase);
+    }
+
+    public void saveNote(Note note)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("title", note.getTitle());
+        contentValues.put("body", note.getText());
+
+        db.insert("NOTES", null, contentValues);
+        db.close();
     }
 }
--- src/main/java/com/example/notes/views/NoteActivity.java
+++ src/main/java/com/example/notes/views/NoteActivity.java
@@ -3,24 +3,56 @@ package com.example.notes.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.notes.R;
+import com.example.notes.databases.NotesDatabase;
+import com.example.notes.models.Note;
 
 public class NoteActivity extends AppCompatActivity
 {
-
+    private EditText noteTitleEditText, noteTextEditText;
+    private NotesDatabase notesDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        noteTitleEditText = findViewById(R.id.note_title_text_input);
+        noteTextEditText = findViewById(R.id.note_text_text_input);
+
+        notesDatabase = new NotesDatabase(this);
     }
 
     @Override
-    protected void onResume()
+    protected void onPause()
     {
-        super.onResume();
-        saveNote();
+        super.onPause();
+
+        createAndSaveNote();
     }
+
+    private void createAndSaveNote()
+    {
+
+        if (fieldsContainText(noteTitleEditText, noteTextEditText)) {
+
+            Note newNote = new Note(noteTitleEditText.getText().toString(), noteTextEditText.getText().toString());
+            saveNoteToDatabase(newNote);
+
+        }
+    }
+
+    private boolean fieldsContainText(EditText editText, EditText editText2)
+    {
+        return (editText.getText().toString() != null) && (editText2.getText().toString() != null);
+    }
+
+    private void saveNoteToDatabase(Note note)
+    {
+        notesDatabase.saveNote(note);
+    }
+
 }
